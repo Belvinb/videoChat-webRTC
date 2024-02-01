4e311bb4e9f5b277b7002507f6659c9922ce98de
Belvin-Benny <belvin.benny@harvestinfosoft.com>
2024-02-01 21:42:53 +0530

    videochat app

---
+++ client/src/screens/Lobby.jsx
@@ -0,0 +1,57 @@
+import { useState, useCallback, useEffect } from "react";
+import { useSocket } from "../context/SocketProvider";
+import { useNavigate } from "react-router-dom";
+
+const LobbyScreen = () => {
+  const [email, setEmail] = useState("");
+  const [room, setRoom] = useState("");
+  const socket = useSocket();
+  const navigate = useNavigate()
+
+  const handleRoomJoin = useCallback((data) => {
+    const { email, room } = data;
+    console.log(email, room);
+    navigate(`/room/${room}`)
+  }, [navigate]);
+
+  const handleFormSubmit = useCallback(
+    (e) => {
+      e.preventDefault();
+      socket.emit("room:join", { email, room });
+    },
+    [email, room, socket]
+  );
+
+  useEffect(() => {
+    socket.on("room:join", handleRoomJoin);
+    return () => {
+      socket.off("room:join", handleRoomJoin);
+    };
+  }, [socket,handleRoomJoin]);
+
+  return (
+    <div>
+      lobby
+      <form onSubmit={handleFormSubmit}>
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <br />
+        <label htmlFor="room">Room Number</label>
+        <input
+          type="text"
+          id="room"
+          value={room}
+          onChange={(e) => setRoom(e.target.value)}
+        />
+        <br />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+export default LobbyScreen;
